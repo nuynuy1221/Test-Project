c436f4b551debcdd9a123ad41f56e5911c04c90e
nuynuy1221 <nuynuy1221@gmail.com>
2025-12-10 00:39:17 +0700

    Create AntiAFK.lua

---
+++ AntiAFK.lua
@@ -0,0 +1,43 @@
+repeat task.wait() until game:IsLoaded()
+task.wait(2)
+
+repeat task.wait() until game:IsLoaded()
+local targetPlace = 16277809958
+if game.PlaceId ~= targetPlace then
+    warn("PlaceId ไม่ตรง ไม่ Anti-AFK ให้")
+    return
+end
+
+task.spawn(function()
+    while true do
+
+        local GuiService = game:GetService("GuiService")
+        local VirtualInputManager = game:GetService("VirtualInputManager")
+        local button = game:GetService("Players").LocalPlayer.PlayerGui.Guides.List.StageInfo.Buttons.StageInfo.Button
+    
+        button.Selectable = true
+        GuiService.SelectedCoreObject = button
+        
+        VirtualInputManager:SendKeyEvent(true,Enum.KeyCode.Return,false,game)
+        VirtualInputManager:SendKeyEvent(false,Enum.KeyCode.Return,false,game)
+        
+        wait(0.1)
+        GuiService.SelectedCoreObject = nil
+        
+        wait(15)
+        
+        local GuiService = game:GetService("GuiService")
+         local VirtualInputManager = game:GetService("VirtualInputManager")
+        local button = game:GetService("Players").LocalPlayer.PlayerGui.StageInfo.Holder.Back.Button
+        
+        button.Selectable = true
+        GuiService.SelectedCoreObject = button
+        
+        VirtualInputManager:SendKeyEvent(true,Enum.KeyCode.Return,false,game)
+        VirtualInputManager:SendKeyEvent(false,Enum.KeyCode.Return,false,game)
+        
+        wait(0.1)
+        GuiService.SelectedCoreObject = nil
+        task.wait(15)
+    end
+end)
