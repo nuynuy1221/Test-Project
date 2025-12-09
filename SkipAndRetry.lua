bbbd3a5be946a501e890561bc490c1603e0eb506
nuynuy1221 <nuynuy1221@gmail.com>
2025-12-09 23:00:09 +0700

    SkipAndRetry.lua

--- SkipAndRetry.lua
+++ SkipAndRetry.lua
@@ -1,20 +1,22 @@
-while true do
-    local args = {
-        [1] = "Retry"
-    }
-
-    game:GetService("ReplicatedStorage")
-        :WaitForChild("Networking")
-        :WaitForChild("EndScreen")
-        :WaitForChild("VoteEvent")
-        :FireServer(unpack(args))
-
-    wait(1)
-
-    local args = {
-        [1] = "Skip"
-    }
-
-    game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("SkipWaveEvent"):FireServer(unpack(args))
-    task.wait(30) -- รอ 30 วิแล้วลูปใหม่
-end
+task.spawn(function()
+    while true do
+        local args = {
+            [1] = "Retry"
+        }
+            
+        game:GetService("ReplicatedStorage")
+            :WaitForChild("Networking")
+            :WaitForChild("EndScreen")
+            :WaitForChild("VoteEvent")
+            :FireServer(unpack(args))
+            
+        wait(1)
+            
+        local args = {
+            [1] = "Skip"
+        }
+            
+        game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("SkipWaveEvent"):FireServer(unpack(args))
+        task.wait(30) -- รอ 30 วิแล้วลูปใหม่
+    end
+end)
