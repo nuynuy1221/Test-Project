5938cb1c1cea6ff10f0a29985fc9b0619a9eb475
nuynuy1221 <nuynuy1221@gmail.com>
2025-12-11 21:16:06 +0700

    Update AntiAFK.lua

--- AntiAFK.lua
+++ AntiAFK.lua
@@ -1,18 +1,12 @@
 repeat task.wait() until game:IsLoaded()
 task.wait(2)
 
-local targetPlace = 16277809958
-if game.PlaceId ~= targetPlace then
-    warn("PlaceId ไม่ตรง ไม่ Anti-AFK ให้")
-    return
-end
-
 task.spawn(function()
     while true do
 
         local GuiService = game:GetService("GuiService")
         local VirtualInputManager = game:GetService("VirtualInputManager")
-        local button = game:GetService("Players").LocalPlayer.PlayerGui.Guides.List.StageInfo.Buttons.StageInfo.Button
+        local button = game:GetService("CoreGui").TopBarApp.TopBarApp.UnibarLeftFrame.UnibarMenu["2"]["3"].chat.IconHitArea_chat
     
         button.Selectable = true
         GuiService.SelectedCoreObject = button
