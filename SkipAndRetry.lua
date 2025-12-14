dd894fe70c3d2f29c6c9b97fc42adf308216ab49
nuynuy1221 <nuynuy1221@gmail.com>
2025-12-14 15:43:17 +0700

    Update SkipAndRetry.lua

--- SkipAndRetry.lua
+++ SkipAndRetry.lua
@@ -3,52 +3,44 @@ task.wait(2)
 
 local targetPlace = 16277809958
 if game.PlaceId ~= targetPlace then
-    warn("PlaceId ไม่ตรง ไม่ Skip และ Retry ให้")
     return
 end
 
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Networking = ReplicatedStorage:WaitForChild("Networking")
 
-local GuiService = game:GetService("GuiService")
-local VirtualInputManager = game:GetService("VirtualInputManager")
-
--- ฟังก์ชันกดปุ่ม Skip
+-- =========================
+-- ฟังก์ชัน Skip Wave
+-- =========================
 local function pressSkipButton()
-    local success, button = pcall(function()
-        return playerGui.SkipWave.Holder.Yes.Button
+    local args = {
+        [1] = "Skip"
+    }
+
+    pcall(function()
+        Networking:WaitForChild("SkipWaveEvent"):FireServer(unpack(args))
     end)
-    
-    if success and button then
-        button.Selectable = true
-        GuiService.SelectedCoreObject = button
-
-        -- กด Enter
-        VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
-        VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
-
-        wait(0.1)
-        GuiService.SelectedCoreObject = nil
-    end
 end
 
+-- =========================
 -- ฟังก์ชัน Vote Retry
+-- =========================
 local function pressRetryButton()
-    local RetryMap = {
+    local args = {
         [1] = "Retry"
     }
 
     pcall(function()
-        game:GetService("ReplicatedStorage")
-            :WaitForChild("Networking")
-            :WaitForChild("EndScreen")
-            :WaitForChild("VoteEvent")
-            :FireServer(unpack(RetryMap))
+        Networking:WaitForChild("EndScreen"):WaitForChild("VoteEvent"):FireServer(unpack(args))
     end)
 end
 
--- เช็คปุ่มทุก 2 วิ
+-- =========================
+-- Loop ทุก 2 วินาที
+-- =========================
 task.spawn(function()
     while true do
         task.wait(2)
