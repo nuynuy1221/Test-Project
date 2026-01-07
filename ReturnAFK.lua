84962be6a0d64d08823837584cf5cefc75b80623
nuynuy1221 <nuynuy1221@gmail.com>
2026-01-08 02:07:26 +0700

    Update ReturnAFK.lua

--- ReturnAFK.lua
+++ ReturnAFK.lua
@@ -1,5 +1,5 @@
 repeat task.wait() until game:IsLoaded()
-task.wait(30)
+task.wait(1)
 
 -- =======================
 -- เช็ค PlaceId ก่อนเสมอ
@@ -10,30 +10,8 @@ if game.PlaceId ~= targetPlace then
     return
 end
 
-local player = game:GetService("Players").LocalPlayer
-local GuiService = game:GetService("GuiService")
-local VirtualInputManager = game:GetService("VirtualInputManager")
+local AFKReturn = {
+    [1] = "TeleportMain"
+}
 
--- หา Button จาก GUI
-local button
-pcall(function()
-    button = player.PlayerGui:FindFirstChild("Main") 
-             and player.PlayerGui.Main:FindFirstChild("Create")
-             and player.PlayerGui.Main.Create:FindFirstChild("Button")
-end)
-
-if button then
-    button.Selectable = true
-    GuiService.SelectedCoreObject = button
-
-    -- กดปุ่ม Enter
-    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
-    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
-
-    task.wait(0.1)
-    GuiService.SelectedCoreObject = nil
-
-    print("[แจ้งเตือน] ✅ กดปุ่มสำเร็จ")
-else
-    warn("[แจ้งเตือน] ❌ หา Button ไม่เจอ")
-end
+game:GetService("ReplicatedStorage").Networking.AFKEvent:FireServer(unpack(AFKReturn))
